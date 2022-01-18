712c5c3a0ebe31604ec92b3f5f45bd465cff3098
spalomas <sergi.palomas@bsc.es>
2022-01-19 00:58:11 +0100

    change ALU opcode

--- Project/alu.v
+++ Project/alu.v
@@ -1,7 +1,7 @@
 `include "header.vh"
 
 module alu(
-    input [4:0] op, 
+    input [5:0] op, 
     input [(`REG_FILE_WIDTH-1):0] x, y,
     output reg [(`REG_FILE_WIDTH-1):0] w,
     output reg  cmp);
@@ -14,27 +14,26 @@ always @(*) begin
 	cmp <= 0;
 	case (op)
         // Arithmetic operations
-        5'b00000: w <= x + y;      // 0: ADD 
-        5'b00001: w <= x - y;      // 1: SUB 
-    	5'b00010: w <= x * y;      // 2: MUL
+        5'h00: w <= x + y;      // 0: ADD 
+        5'h01: w <= x - y;      // 1: SUB 
+    	5'h02: w <= x * y;      // 2: MUL
 
         // Bit-wise operations
-        5'b00011: w <= x | y;      // 3: OR
-        5'b00100: w <= x & y;      // 4: AND
-        5'b00101: w <= x ^ y;      // 5: XOR
+        5'h03: w <= x | y;      // 3: OR
+        5'h04: w <= x & y;      // 4: AND
+        5'h05: w <= x ^ y;      // 5: XOR
 
         // Shift operations
-        5'b00110: w <= x << y;     // 6: SLL
-        5'b00111: w <= x >> y;     // 7: SRL
+        5'h06: w <= x << y;     // 6: SLL
+        5'h07: w <= x >> y;     // 7: SRL
 
         // Relational operations
-        5'b01001: cmp <= (x < y);  // 9: LT
-        5'b01010: cmp <= (x > y);  // 10: GT
-        5'b01011: cmp <= (x == y); // 11: EQ
+        5'h09: cmp <= (x < y);  // 9: LT
+        5'h0A: cmp <= (x > y);  // 10: GT
+        5'h0B: cmp <= (x == y); // 11: EQ
 
-        
-        5'b01100: w <= x + y;      // 12: JMP TODO!
-        5'b01000: w <= x | 0;      // 8: MV
+        // I think mem stuff (mv, ld, st...) should be somewhere else
+        5'h0C: w <= x | 0;      // 8: MV
         //5'b110010: TLBWrite 
 
 		default: 
--- Project/alu_tb.v
+++ Project/alu_tb.v
@@ -2,7 +2,7 @@
 `include "alu.v"
 
 module alu_tb;
-    reg [4:0] op;
+    reg [5:0] op;
     reg [(`REG_FILE_WIDTH-1):0] x, y;
     wire [(`REG_FILE_WIDTH-1):0] w;
     wire cmp;
@@ -18,20 +18,20 @@ initial begin
     x=32'h0000_0001;
     y=32'h0000_0001;
 
-    op=5'b00000;
+    op=0;
     for (i = 1; i < 13; i=i+1) begin
         #5;
-        op = op + 1'b1;
+        op = op + 1;
         $display(op);
     end
 
     x=32'h0000_0010;
     y=32'h0000_00F1;
 
-    op=5'b00000;
+    op=0;
     for (i = 1; i < 13; i=i+1) begin
-        #10;
-        op = op + 5'b00001;
+        #5;
+        op = op + 1;
     end
 end
 
