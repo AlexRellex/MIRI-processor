2479bb533933d21d09e910b4eff2f945cde73579
spalomas <sergi.palomas@bsc.es>
2022-01-22 12:46:03 +0100

    iTLB v1

--- Project/Monocycle/iTLB.v
+++ Project/Monocycle/iTLB.v
@@ -4,17 +4,17 @@
 
 module iTLB(  
     // SYSTEM  
-    input reset, clk,
+    input clk,
+    input reset,
 
     // INPUT
     input [(`VIRT_ADDR_WIDTH-1):0] VirtualAddr,
-    //input supervisor_mode,
     input tlb_write,
-    input [(`PHY_ADDR_WIDTH-1):0] physical_page_num_mem,
+    input [(`PHY_PAGE_NUM_WIDTH-1):0] physical_page_num_mem,
 
     // OUTPUT
-    output [(`PHY_ADDR_WIDTH-1):0] PhysicalAddr,
-    output tlb_miss);
+    output reg [(`PHY_ADDR_WIDTH-1):0] PhysicalAddr,
+    output reg tlb_miss);
 
 
     /*
@@ -23,60 +23,60 @@ module iTLB(
     */
 
     // As with iCache, we define 4 lines for the TLB
-    reg [(`PHY_ADDR_WIDTH-1):0] page_table [(`ITLB_NUM_LINES-1):0];
-    reg [(`PHY_PAGE_NUM_WIDTH-1):0] page_translation [(`ITLB_NUM_LINES-1):0];
+    reg [(`PHY_ADDR_WIDTH-1):0] tlb_tag [(`ITLB_NUM_LINES-1):0];
+    reg [(`PHY_PAGE_NUM_WIDTH-1):0] tlb_translation [(`ITLB_NUM_LINES-1):0];
     reg valid_page [(`ITLB_NUM_LINES-1):0];
-    reg [1:0] countValid [(`ITLB_NUM_LINES-1):0];
+
     wire [(`PHY_ADDR_WIDTH-1):0] page_tag;
     wire [(`PAGE_SIZE-1):0] page_offset;
-    wire tlb_hit;
 
+    assign page_tag = VirtualAddr [(`VIRT_ADDR_WIDTH-1):`PAGE_SIZE];
+    assign page_offset = VirtualAddr [(`PAGE_SIZE-1):0];
+
+    integer line;
+
+    initial begin
+        $display("start iTLB with 0s");
+        for (line=0; line<`ITLB_NUM_LINES; line=line+1) begin
+             valid_page[line] = 0;
+        end
+        valid_page[0] = 1;
+        tlb_tag[0] = 20'h00110;
+        tlb_translation[0] = 8'hAB;
+	end
 
     always @ (posedge clk) begin
-        
-        page_tag = VirtualAddr [(`VIRT_ADDR_WIDTH-1):`PAGE_SIZE];
-        page_offset =  VirtualAddr [(`PAGE_SIZE-1):0];
-        if reset begin
-            for (i=0; i <=1048575; i++ ) begin
-                page_table[i] = 20'b0;
-                tlb_miss = 0;
+        if (reset) begin
+            for (line=0; line<`ITLB_NUM_LINES; line=line+1) begin
+                valid_page[line] = 0;
             end
-        end        
-        // Loop for all iTLB lines
-        for (line=0; line < `ITLB_NUM_LINES; line++) begin
-            // Does the iTLB have the tag we are looking for?
-            if (page_tag [line*20+19:line*20] == page_tag) begin
-                $display("iTLB: Page tag hit")
+        end
+        tlb_miss = 1;
+        // If we are not writting to the iTLB
+        if (!tlb_write) begin
+            // Loop for all iTLB lines
+            for (line=0; line < (`ITLB_NUM_LINES); line=line+1) begin
                 // Is that line valid?
-                if (instValid [line] == 1'b1) begin
-                    $display("iTLB: Valid line")
-                    PhysicalAddr[(`PHY_ADDR_WIDTH-1):`PAGE_SIZE] = page_traduction [line]; 
-                    PhysicalAddr[(`PAGE_SIZE-1):0] = page_offset;
-                    tlb_hit=1;
+                if (valid_page [line] == 1'b1) begin
+                    // Does the iTLB have the tag we are looking for?
+                    if (tlb_tag[line] == page_tag) begin
+                        $display("iTLB: Page hit");
+                        // Get page translation from iTLB
+                        PhysicalAddr = {tlb_translation[line], page_offset};
+                        tlb_miss = 0;
+                    end
                 end
             end
         end
-
-        tlb_miss = ! tlb_hit;
-
-        // Recieve translation from memory
-        else if (tlb_write == 1'b1) begin
-            // If all lines in the iTLB are valid, the first entry is taken by default (TODO: think a better strategy)
-            integer line_to_overwrite = 0; //default
-            // If we want to writo to the iTLB, try to take a line that it is not valid
-            for (line=0; line < 4; line++) begin
-                if (valid_page [line] == 1'b0) begin
-                    line_to_overwrite = line;
-                end
+        // If we don't have a Hit, go ask to memory
+        if (tlb_miss) begin
+            $display("iTLB: Miss. Go to memory");
+            if (tlb_write) begin
+                line = 0; // Write to line 0 by default. TODO: Think a better strategy.
+                tlb_tag[line] = page_tag;
+                tlb_translation[line] = physical_page_num_mem;
             end
-            
-            // Keep tag for future translations
-            page_translation [line_to_overwrite*8+7:line_to_overwrite*8] = page_tag;
-            // Physical translation recieved from main memory
-            page_table [line_to_overwrite*20+19:line_to_overwrite*20] = physical_page_num_mem;
-            // This entry is now valid
-            instValid [line_to_overwrite] = 1'b1;
         end
     end
 
-endmodule // insttructionMem
+endmodule
--- Project/Monocycle/iTLB_tb.v
+++ Project/Monocycle/iTLB_tb.v
@@ -9,21 +9,19 @@ module iTLB_tb;
     reg [(`VIRT_ADDR_WIDTH-1):0] VirtualAddr;
     //input supervisor_mode,
     reg tlb_write;
-    reg [(`PHY_ADDR_WIDTH-1):0] physical_page_num_mem;
+    reg [(`PHY_PAGE_NUM_WIDTH-1):0] physical_page_num_mem;
 
     // OUTPUT
     wire [(`PHY_ADDR_WIDTH-1):0] PhysicalAddr;
     wire tlb_miss;
 
     // As with iCache, we define 4 lines for the TLB
-    reg [(`PHY_ADDR_WIDTH-1):0] page_table [(`ITLB_NUM_LINES-1):0];
-    reg [(`PHY_PAGE_NUM_WIDTH-1):0] page_translation [(`ITLB_NUM_LINES-1):0];
+    reg [(`PHY_ADDR_WIDTH-1):0] tlb_tag [(`ITLB_NUM_LINES-1):0];
+    reg [(`PHY_PAGE_NUM_WIDTH-1):0] tlb_translation [(`ITLB_NUM_LINES-1):0];
     reg valid_page [(`ITLB_NUM_LINES-1):0];
-    reg [1:0] countValid [(`ITLB_NUM_LINES-1):0];
+
     wire [(`PHY_ADDR_WIDTH-1):0] page_tag;
     wire [(`PAGE_SIZE-1):0] page_offset;
-    wire tlb_hit;
-
 
     iTLB uut(clk, reset, VirtualAddr, tlb_write, physical_page_num_mem, PhysicalAddr, tlb_miss);
 
@@ -32,11 +30,22 @@ module iTLB_tb;
     initial begin
         clk = 0;
         reset = 0;
+        tlb_write = 0;
 
         $dumpfile("iTLB_tb.vcd");
         $dumpvars(0, iTLB_tb);
+        
+        VirtualAddr=32'h00FF_0101; #20;
+        
+        VirtualAddr=32'h0011_0101; #20;
 
-        #20;
+        VirtualAddr=32'h0021_0101;
+        physical_page_num_mem=8'h11; #20;
+
+        tlb_write = 1; #20;
+        tlb_write=0; #40;
+
+        reset = 1; #20;
 
         $finish();
         $display("Test OK!");
