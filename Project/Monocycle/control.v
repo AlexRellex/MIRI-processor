92fb3e3a7c628881c64c9c037d1a99aee309bf71
spalomas <sergi.palomas@bsc.es>
2022-01-23 16:52:40 +0100

    reduce control functionality to the minimum

--- Project/Monocycle/control.v
+++ Project/Monocycle/control.v
@@ -8,25 +8,15 @@ module control (
 
     // INPUT
     input [31:0] instruction,
-    input block_pipe_data_cache,
-    input block_pipe_instr_cache,
 
     // OUTPUT
-    output reg ALU_REG_DEST,
-    output reg is_branch,
-    output reg MEM_R_EN, MEM_W_EN,
-    output reg MEM_TO_REG,
-    output reg WB_EN,
     output reg [1:0] ALU_OP,
     output reg [4:0] regA, regB, regD,
-    output reg EN_REG_FETCH, EN_REG_DECODE, EN_REG_ALU, EN_REG_MEM,
-    output reg is_immediate,
-    output reg [31:0] inject_nop,
-    output reg injecting_nop
+    output reg is_immediate
     );
 
     // R-type               M-Type              B-Type
-    //31-25 Operation       31-25 Operation     31-25 Operation
+    //31-25 opcode          31-25 opcode     31-25 opcode
     //24-20 dst             24-20 dst           24-20 off_hi
     //19-15 src1            19-15 src1          19-15 src1
     //14-10 scr2            14-0 Offset         14-10 src2/off_mem
@@ -45,25 +35,20 @@ module control (
     jump            0x31
     */
 
-    wire signed [5:0] operation;
-    assign operation = instruction [31:25];
-    reg [5:0] default_alu_op = 6'b000000; // ADD
+    wire signed [5:0] opcode;
+    assign opcode = instruction [31:25];
+    reg [1:0] default_alu_op = 2'b00; // ADD
+
 
     //wire [4:0] regD,regB;
 
     always @ ( * ) begin 
 
-        case({operation})
+        case({opcode})
             // R-type
             6'b000000, 6'b000001: begin
                 $display("ADD/SUM");
-                WB_EN <= 1;
-                ALU_REG_DEST <= 1;
-                is_branch <= 0;
-                MEM_R_EN <= 0;
-                MEM_W_EN <= 0;
-                MEM_TO_REG <= 0;
-                ALU_OP <= operation;
+                ALU_OP <= 2'b10;
                 regD <= instruction[24:20];
                 regA <= instruction[19:15];
                 regB <= instruction[14:10];
@@ -73,13 +58,7 @@ module control (
             // MUL
             6'b000010: begin
                 $display("MUL");
-                WB_EN <= 1;
-                ALU_REG_DEST <= 1;
-                is_branch <= 0;
-                MEM_R_EN <= 0;
-                MEM_W_EN <= 0;
-                MEM_TO_REG <= 0;
-                ALU_OP <= operation;
+                ALU_OP <= default_alu_op;
                 regD <= instruction[24:20];
                 regA <= instruction[19:15];
                 regB <= instruction[14:10];
@@ -89,12 +68,6 @@ module control (
             // LDB, LDW
             6'b010000, 6'b010001: begin
                 $display("LD");
-                WB_EN <= 1;
-                ALU_REG_DEST <= 0;
-                is_branch <= 0;
-                MEM_R_EN <= 1;
-                MEM_W_EN <= 0;
-                MEM_TO_REG <= 1;
                 ALU_OP <= default_alu_op;
                 regA <= instruction[19:15];
                 regB <= instruction[24:20]; // dst
@@ -103,150 +76,17 @@ module control (
             // STB, STW
             6'b010010, 6'b010011: begin
                 $display("STR");
-                WB_EN <= 0;
-                ALU_REG_DEST <= 1;
-                is_branch <= 0;
-                MEM_R_EN <= 0;
-                MEM_W_EN <= 1;
-                MEM_TO_REG <= 0;
                 ALU_OP <= default_alu_op;
                 regA <= instruction[24:20];
                 regB <= instruction[19:15];
                 is_immediate  <= 1;
             end
-            //Move
-            6'b010100: begin
-                WB_EN <= 1;
-                ALU_REG_DEST <= 0;
-                is_branch <= 0;
-                MEM_R_EN <= 0;
-                MEM_W_EN <= 0;
-                MEM_TO_REG <= 0;
-                ALU_OP <= default_alu_op;
-                regA <= instruction[24:20];
-                regB <= instruction[19:15];
-                regD <= 6'b000000;
-                is_immediate <= 0;
-            end
-            // BEQ
-            6'b110000: begin
-                WB_EN <= 1;
-                ALU_REG_DEST <= 1;
-                is_branch <= 1;
-                MEM_R_EN <= 0;
-                MEM_W_EN <= 0;
-                MEM_TO_REG <= 0;
-                ALU_OP <= 2'b01;
-                regD <= 6'b000000;
-                regB <= instruction[20:16];
-                is_immediate  <= 1;
-            end
-            // Jump
-            6'b110001: begin
-                WB_EN <= 0;
-                ALU_REG_DEST <= 1;
-                is_branch <= 1;
-                MEM_R_EN <= 0;
-                MEM_W_EN <= 0;
-                MEM_TO_REG <= 0;
-                ALU_OP <= 2'b01;
-                regD <= 6'b000000;
-                regB <= instruction[20:16];
-                is_immediate <= 0;
-            end
+
             default: begin
-                WB_EN <= 0;
-                ALU_REG_DEST <= 0;
-                is_branch <= 0;
-                MEM_R_EN <= 0;
-                MEM_W_EN <=0;
-                MEM_TO_REG <=0;
                 ALU_OP <= 2'b10;
                 is_immediate  <= 0;
             end
         endcase
 
-        if (block_pipe_instr_cache == 1'b1) begin
-            EN_REG_FETCH = 0;
-            EN_REG_DECODE = 1;
-            EN_REG_ALU = 1;
-            EN_REG_MEM = 1;
-            injecting_nop = 1'b1;
-             WB_EN =0; 
-        end
-        else begin
-            EN_REG_FETCH = 1;
-            injecting_nop = 1'b0;
-        end
-
-        if (block_pipe_data_cache  == 1'b1) begin
-            EN_REG_FETCH = 0;
-            EN_REG_DECODE = 0;
-            EN_REG_ALU = 0;
-            EN_REG_MEM = 0;
-            WB_EN =0; 
-        end
-        else if (block_pipe_data_cache  == 1'b0 && block_pipe_instr_cache == 1'b0) begin
-            EN_REG_FETCH = 1;
-            EN_REG_DECODE = 1;
-            EN_REG_ALU = 1;
-            EN_REG_MEM = 1;     
-        end
-    end 
-    always @ (reset) begin
-
-        if (reset == 1) begin
-            EN_REG_FETCH = 1;
-            EN_REG_DECODE = 1;
-            EN_REG_ALU = 1;
-            EN_REG_MEM = 1;
-            inject_nop= 32'b0;
-            injecting_nop = 1'b0;
-
-        end
-
-        // Block pipeline for iCache
-        if (block_pipe_instr_cache == 1'b1) begin
-            EN_REG_FETCH = 0;
-            EN_REG_DECODE = 1;
-            EN_REG_ALU = 1;
-            EN_REG_MEM = 1;
-            injecting_nop = 1'b1;
-             WB_EN =0; 
-        end
-        else begin
-            EN_REG_FETCH = 1;
-            injecting_nop = 1'b0;
-        end
-
-
-        // Block pipeline for dCache miss
-        if (block_pipe_data_cache) begin
-            EN_REG_FETCH = 0;
-            EN_REG_DECODE = 0;
-            EN_REG_ALU = 0;
-            EN_REG_MEM = 0;
-            WB_EN =0;
-        end
-
-        // If no need to block pipeline
-        else if (!block_pipe_data_cache && !block_pipe_instr_cache) begin
-            EN_REG_FETCH = 1;
-            EN_REG_DECODE = 1;
-            EN_REG_ALU = 1;
-            EN_REG_MEM = 1;     
-        end
-    end
-
-    always @ (reset) begin
-
-        if (reset == 1) begin
-            EN_REG_FETCH = 1;
-            EN_REG_DECODE = 1;
-            EN_REG_ALU = 1;
-            EN_REG_MEM = 1;
-            inject_nop = 32'b0;
-            injecting_nop = 1'b0;
-        end
     end 
 endmodule // register
