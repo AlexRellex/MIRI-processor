3a4fb2588b487570d44866d1b2d57f844a51a7d5
spalomas <sergi.palomas@bsc.es>
2022-01-23 20:46:27 +0100

    fetch_stage compiling

--- Project/fetch_stage_tb.v
+++ Project/fetch_stage_tb.v
@@ -10,10 +10,10 @@ module fetch_stage_tb;
     reg [(`ICACHE_LINE_WIDTH-1):0] instr_from_mem; // Instruction from memory when cache misses
     reg mem_data_rdy;
     reg data_filled_ack;
-    wire reg [(`VIRT_ADDR_WIDTH-1):0] PCnext, // PC for the next cycle
-    wire reg [(`VIRT_ADDR_WIDTH-1):0] instruction; // Instruction from Icache to next stage
-    wire reg reqI_mem; // Initiate request to memory
-    wire reg [(`ICACHE_TAG_WIDTH-1):0] reqAddrI_mem;
+    wire [(`VIRT_ADDR_WIDTH-1):0] PCnext; // PC for the next cycle
+    wire [(`VIRT_ADDR_WIDTH-1):0] instruction; // Instruction from Icache to next stage
+    wire reqI_mem; // Initiate request to memory
+    wire [(`MEM_ADDRESS_LEN-1):0] reqAddrI_mem;
 
     fetch_stage uut(clk, reset, PCbranch, branch_hit, wrt_en, 
     instr_from_mem, mem_data_rdy, data_filled_ack, 
@@ -29,9 +29,9 @@ always #5 clk = ~clk;
         $dumpfile("fetch_stage.vcd");
         $dumpvars(0, fetch_stage_tb);
 
-        [(`VIRT_ADDR_WIDTH-1):0] PCbranch = 0x1000;
+        PCbranch = 32'h0000_0010;
         branch_hit = 0;
-        [(`ICACHE_LINE_WIDTH-1):0] instr_from_mem = ;
+        instr_from_mem = 128'h0000_0000_0000_0000_0000_0000_0000_0000;
         mem_data_rdy = 0;
         data_filled_ack = 0;
 
